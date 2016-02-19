30115dcb8e8e221db8616c2f635cb5799b84ff34
Alex MacLean <alex.maclean2000@gmail.com>
2016-02-18 22:13:41 -0800

    added countFactors method

--- CompetitionLibrary/CompetitionLibrary/Program.cs
+++ CompetitionLibrary/CompetitionLibrary/Program.cs
@@ -8,6 +8,15 @@ namespace CompetitionLibrary
 {
     class Program
     {
+        //returns the number of factors in a number including 1 and the number
+        public static int countFactors(int number)
+        {
+            int factors = 0;
+            for (int i = 1; i <= number; ++i)
+                factors += number % i == 0 ? 1 : 0;
+            return factors;
+        }
+
         static void Main(string[] args)
         {
         }
