79ed766b3deb551f6ecb1d28c59a7de4b4b11131
Tobias-Rohde <tobias_rohde@outlook.com>
2016-02-19 16:37:47 -0800

    Added files via upload

---
+++ CompetitionLibrary/CompetitionLibrary/Combinatorics.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+class Combinatorics
+{
+    /// <summary>
+    /// Returns factorial of n.
+    /// </summary>
+    public static BigInteger factorial(BigInteger n)
+    {
+        return n == 0 ? 1 : factorial(n - 1) * n;
+    }
+
+    /// <summary>
+    /// Returns Binomial Coefficient of n and k (nCk).
+    /// </summary>
+    public static BigInteger BinomialCoefficient(BigInteger n, BigInteger k)
+    {
+        return factorial(n) / (factorial(n - k) * factorial(k));
+    }
+
+    /// <summary>
+    /// Returns all subsets of k, disregarding order.
+    /// </summary>
+    public static List<List<T>> Combinations<T>(List<T> list)
+    {
+        var result = new List<List<T>>();
+
+        for (BigInteger bitMask = 0; bitMask < (BigInteger.One << list.Count); ++bitMask) {
+            var subset = new List<T>();
+            for (int i = 0; i < list.Count; ++i) {
+                if ((bitMask >> i & 1) == 1) {
+                    subset.Add(list[i]);
+                }
+            }
+            result.Add(subset);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Returns all subsets with length k of the list, disregarding order.
+    /// </summary>
+    public static List<List<T>> KCombinations<T>(List<T> list, int k)
+    {
+        var result = new List<List<T>>();
+        int n = list.Count;
+
+        List<int> indices = new List<int>(Enumerable.Range(0, k));
+
+        while (true) {
+            nextSubset: result.Add(indices.Select(i => list[i]).ToList());
+
+            for (int i = k - 1; i >= 0; --i) {
+                if (indices[i] < n - k + i) {
+                    indices[i] += 1;
+
+                    for (int j = i + 1; j < k; ++j) {
+                        indices[j] = indices[i] + j - i;
+                    }
+
+                    goto nextSubset;
+                }
+            }
+
+            break;
+        }
+
+        return result;
+    }
+}
