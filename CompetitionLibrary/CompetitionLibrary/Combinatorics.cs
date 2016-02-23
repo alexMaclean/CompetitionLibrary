e6b05d3b9214f4dd07d5ccb332a8ecf475d46456
Tobias-Rohde <tobias_rohde@outlook.com>
2016-02-23 13:17:42 -0800

    Added permutations; still have to refactor stuff.

--- CompetitionLibrary/CompetitionLibrary/Combinatorics.cs
+++ CompetitionLibrary/CompetitionLibrary/Combinatorics.cs
@@ -70,4 +70,63 @@ class Combinatorics
 
         return result;
     }
+    
+    /// <summary>
+    /// Swaps lhs with rhs.
+    /// </summary>
+    static void Swap<T>(ref T lhs, ref T rhs)
+	{
+	    T temp;
+	    temp = lhs;
+	    lhs = rhs;
+	    rhs = temp;
+	}
+	
+	/// <summary>
+    /// Swaps the value of list and index1 with the value at index2.
+    /// </summary>
+	static void Swap<T>(List<T> list, int index1, int index2) {
+		T temp = list[index1];
+		list[index1] = list[index2];
+		list[index2] = temp;
+	}
+    
+    /// <summary>
+    /// Returns all permutations of the list.
+    /// </summary>
+	public static List<List<T>>Permutations<T>(List<T> list) {
+		var result = new List<List<T>>();
+        int n = list.Count;
+
+        List<int> indices = new List<int>(Enumerable.Range(0, n));
+        
+
+        while(true) {
+        	result.Add(indices.Select(i => list[i]).ToList());
+        	
+        	int k = n - 2;
+        	for (;  k >= 0; --k) {
+				if (indices[k] < indices[k + 1]) {
+					break;
+				}
+			}
+			
+			if (k < 0) {
+				break;
+			}
+			
+			int l = n - 1;
+			for (; l > k; --l) {
+				if (indices[k] < indices[l]) {
+					break;
+				}
+			}
+			
+			Swap<int>(indices, k, l);
+			
+			indices.Reverse(k + 1, n - k - 1);
+        }
+
+		return result;
+	}
 }
